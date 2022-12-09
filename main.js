4191ad0ab4aec0393955c65971faca69ec1dea9e
Arturo Pacheco <arturojesus.pc@gmail.com>
2022-12-09 10:12:19 -0400

    merge clase8.html a index.html

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ navEmail.addEventListener("click", showDesktopMenu);
 
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const logoMenu = document.querySelector(".logo-menu");
+logoMenu.addEventListener("click", showMobileMenu);
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
 function showDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+function showMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
