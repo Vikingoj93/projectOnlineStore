4985aa351aee913eaa542b53444147e0bc90919e
Arturo Pacheco <arturojesus.pc@gmail.com>
2022-12-14 15:35:01 -0400

    ...

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const navEmail = document.querySelector("#nav-email");
-navEmail.addEventListener("click", showDesktopMenu);
-
 const desktopMenu = document.querySelector(".desktop-menu");
-
 const logoMenu = document.querySelector(".logo-menu");
-logoMenu.addEventListener("click", showMobileMenu);
-
 const mobileMenu = document.querySelector(".mobile-menu");
+const logoCart = document.querySelector(".logo-cart");
+const cartMenu = document.querySelector(".detail-product")
 
+navEmail.addEventListener("click", showDesktopMenu);
+logoMenu.addEventListener("click", showMobileMenu);
+logoCart.addEventListener("click", showCartMenu);
+
+function showCartMenu() {
+    cartMenu.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+}
 function showDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+    cartMenu.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
 }
 function showMobileMenu() {
     mobileMenu.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
+    cartMenu.classList.add("inactive");
 }
