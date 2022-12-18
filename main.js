dfd5e0edeede24f9ef7283e2f56af8aff290eb81
Arturo Pacheco <arturojesus.pc@gmail.com>
2022-12-18 17:00:37 -0400

    agregado mediaquery con javaScript

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const logoCart = document.querySelector(".logo-cart");
 const cartMenu = document.querySelector(".detail-product")
 
+const mediaqueryListMobile = window.matchMedia("(max-width: 621px)");
+const mediaqueryListDesktop = window.matchMedia("(max-width: 620px)");
+
+mediaqueryListMobile.addListener(()=>{    
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    cartMenu.classList.add("inactive");;
+});
+mediaqueryListDesktop.addListener(()=>{    
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    cartMenu.classList.add("inactive");;
+});
+
 navEmail.addEventListener("click", showDesktopMenu);
 logoMenu.addEventListener("click", showMobileMenu);
 logoCart.addEventListener("click", showCartMenu);
@@ -16,8 +30,8 @@ function showCartMenu() {
 }
 function showDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
-    cartMenu.classList.add("inactive")
-    mobileMenu.classList.add("inactive")
+    cartMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
 }
 function showMobileMenu() {
     mobileMenu.classList.toggle("inactive");
